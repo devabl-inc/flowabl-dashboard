4a18dcfdb53e82da8b2121067f2f01183b850cd8
Timothy R. Bula <trbula@us.ibm.com>
2021-10-11 19:10:20 -0500

    fix: url to docs

--- src/Config/appConfig.tsx
+++ src/Config/appConfig.tsx
@@ -36,7 +36,7 @@ export const AppLink = {
 export const MARKETING_URL = "https://flowabl.io";
 export const APP_URL = "https://app.flowabl.io/apps/flow/workflows";
 export const OSS_URL = "https://useboomerang.io/flow ";
-export const DOCS_URL = "https://useboomerang.io/docs";
+export const DOCS_URL = "https://www.useboomerang.io/docs/boomerang-flow/introduction/overview";
 export const SUPPORT_EMAIL = "hello@flowabl.io";
 export const DEV_URL = "http://localhost:3000";
 
