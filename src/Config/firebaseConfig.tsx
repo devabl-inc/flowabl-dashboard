66b49d5c1ba3252e6cd12f95726f76b8d5bd93eb
Timothy R. Bula <trbula@us.ibm.com>
2021-10-12 17:12:35 -0500

    fix: remove firebase hardcoding

--- src/Config/firebaseConfig.tsx
+++ src/Config/firebaseConfig.tsx
@@ -5,14 +5,16 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(process.env);
 const firebaseConfig = {
-  apiKey: "AIzaSyD8Xj1Ze_sHYwJkOA8K_bJUShS0v4WC3v4",
-  authDomain: "flowabl-dashboard.firebaseapp.com",
-  projectId: "flowabl-dashboard",
-  storageBucket: "flowabl-dashboard.appspot.com",
-  messagingSenderId: "724503666186",
-  appId: "1:724503666186:web:876152ea737ba443fc89f3",
-  measurementId: "G-DJG9QZR3N4",
+  apiKey: window._SERVER_DATA?.FIREBASE_API_KEY || process.env.REACT_APP_FIREBASE_API_KEY,
+  appId: window._SERVER_DATA?.FIREBASE_APP_ID || process.env.REACT_APP_FIREBASE_APP_ID,
+  authDomain: window._SERVER_DATA?.FIREBASE_AUTH_DOMAIN || process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  measurementId: window._SERVER_DATA?.FIREBASE_MEASUREMENT_ID || process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId:
+    window._SERVER_DATA?.FIREBASE_MESSAGING_SENDER_ID || process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: window._SERVER_DATA?.FIREBASE_PROJECT_ID || process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: window._SERVER_DATA?.FIREBASE_STORAGE_BUCKET || process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 };
 
 // Initialize Firebase
