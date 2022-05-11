1154e4000357fd4f663399331fe7509a7158fc92
Timothy Bula <timrbula@gmail.com>
2022-05-10 21:01:53 -0500

    fix: comment out portal code

--- src/Hooks/useAuth.tsx
+++ src/Hooks/useAuth.tsx
@@ -24,7 +24,7 @@ import {
   QueryDocumentSnapshot,
 } from "firebase/firestore";
 import { Tiers } from "Config/appConfig";
-import { auth, db } from "Config/firebaseConfig";
+import { app, auth, db } from "Config/firebaseConfig";
 import { getFunctions, httpsCallable } from "firebase/functions";
 import type { FirebaseError } from "firebase/app";
 import type { DocumentData, DocumentSnapshot } from "firebase/firestore";
@@ -301,16 +301,17 @@ async function checkoutUser(user: User, priceId: string, tier: string, interval:
   );
 }
 
-async function createCustomerPortal() {
-  //TODO: need to pass through the Firebase app?!?!
-  const functions = getFunctions(app, "us-west2");
-  const createPortalLink = await httpsCallable(functions, "ext-firestore-stripe-subscriptions-createPortalLink");
-  //I think subToken needs to handle a type in the token as well of create, upgrade, downgrade, etc
-  const { data } = createPortalLink({ returnUrl: "https://dashboard.flowabl.io?signUpToken=${newSubToken}" });
-  if (data.url) {
-    window.location.assign(data.url);
-  }
-}
+// async function createCustomerPortal() {
+//   //TODO: need to pass through the Firebase app?!?!
+//   const functions = getFunctions(app, "us-west2");
+//   const createPortalLink = await httpsCallable(functions, "ext-firestore-stripe-subscriptions-createPortalLink");
+//   //I think subToken needs to handle a type in the token as well of create, upgrade, downgrade, etc
+//   const { data } = createPortalLink({ returnUrl: "https://dashboard.flowabl.io?signUpToken=${newSubToken}" });
+
+//   if (data.url) {
+//     window.location.assign(data.url);
+//   }
+// }
 
 async function createSignUpToken(email: string, tier: string, interval: string, name: string) {
   try {
